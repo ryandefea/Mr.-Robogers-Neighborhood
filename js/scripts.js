8856e1430c0c80d36c4c52c7e10d50cc63663d4a
ryandefea <ryan.defea@gmail.com>
2022-06-10 14:57:19 -0700

    add if statement for 2.

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,15 @@ function beepBoop(countTo) {
      
     if (i.toString().includes(3)) {
       newArray.push("Won't you be my neighbor?")
-    } else if ()
-
-
-
-
-
+    } else if (i.toString().includes(2)) {
+      newArray.push("Boop")
+    } else if (i.toString().includes(1)) {
+      newArray.push("Beep")
     } else { 
     newArray.push(i);
-    } 
-     
-     
+    }; 
    }
    return newArray;
-
 } 
 const data=beepBoop(5);
 console.log('--', data)
--- readme.md
+++ readme.md
@@ -8,5 +8,13 @@ Test: "It should return "won't you be my neighbor" if a 3 is included"
 Code: beepBoop(5);
 Expected Output: [0, 1, 2, "won't you be my neighbor", 4, 5]
 
+Test: "It should return "Boop" if a 2 is included"
+Code: beepBoop(5);
+Expected Output: [0, 1, "Boop", 3, 4, 5]
+
+Test: "It should return "won't you be my neighbor" if a 1 is included"
+Code: beepBoop(5);
+Expected Output: [0, 1, "Beep", 3, 4, 5]
+
 
 
