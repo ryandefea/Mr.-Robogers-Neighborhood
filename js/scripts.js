cf0b13c34dcae1f73bcc07d172caa6190113d5a7
ryandefea <ryan.defea@gmail.com>
2022-06-10 17:48:08 -0700

    create user interface js.

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,26 @@ function beepBoop(countTo) {
       newArray.push("Boop")
     } else if (i.toString().includes(1)) {
       newArray.push("Beep")
-    } else { newArray.push(i);
+    } else { 
+      newArray.push(i);
     }; 
    }
    return newArray;
-} 
-beepBoop(5);
+}; 
+//beepBoop(5);
 
+$(document).ready(function () {
+  $("form#numbermatch").submit(function(event){
+    const number = parseInt($("input#number").val());
+    event.preventDefault();
+    $("#match").text(beepBoop(number));
+  });
+});
+
+$(document).ready(function () {
+  $("button#hide-match").click(function(event) {
+    event.preventDefault();
+    $("#match").text("");
+    $("#number").val("");
+  });
+});
